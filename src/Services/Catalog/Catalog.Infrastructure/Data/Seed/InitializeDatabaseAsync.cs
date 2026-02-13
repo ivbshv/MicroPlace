c4f5dd44d87283678619e2e413791aeb51a451c9
Bushuev Igor <boommotivator@gmail.com>
2026-02-13 14:00:12 +0300

    добавлен класс InitializeDatabaseAsync для инициализации и наполнения БД тестовыми данными

---
+++ src/Services/Catalog/Catalog.Infrastructure/Data/Seed/InitializeDatabaseAsync.cs
@@ -0,0 +1,38 @@
+﻿using Marten.Schema;
+using Marten;
+
+namespace Catalog.Infrastructure.Data.Seed
+{
+    public class InitializeDatabaseAsync : IInitialData
+    {
+        public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+        {
+            using var session = store.LightweightSession();
+
+            if (!await session.Query<Brand>().AnyAsync())
+            {
+                session.Store<Brand>(InitialData.Brands);
+                await session.SaveChangesAsync(cancellation);
+            }
+
+            foreach (var category in InitialData.Categories)
+            {
+                if(!session.Query<Category>().Any(c => c.Id == category.Id))
+                {
+                    session.Store(category); 
+                }
+            }
+
+            foreach (var item in InitialData.CatalogItems)
+            {
+                if (!session.Query<CatalogItem>().Any(c => c.Id == item.Id))
+                {
+                    session.Store(item); 
+                }
+            }
+
+            await session.SaveChangesAsync(cancellation);
+
+        }
+    }
+}
