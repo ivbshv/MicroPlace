6923a04ec804887f028d7a0397e7041742631f60
Bushuev Igor <boommotivator@gmail.com>
2026-02-13 22:44:00 +0300

    Добавление пакета MediatR в проект, Запрос, обработчик и ответ для получения списка брендов

---
+++ src/Services/Catalog/Catalog.Application/Handlers/Brandhandlers/GetBrandsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Application.Queries.BrandQueries;
+using Catalog.Application.Responses.BrandResponses;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Brandhandlers
+{
+    public class GetBrandsQueryHandler(IBrandRepository brandRepository) : IRequestHandler<GetBrandsQuery, GetBrandsResult>
+    {
+        public async Task<GetBrandsResult> Handle(GetBrandsQuery query, CancellationToken cancellationToken)
+        {
+            IEnumerable<Brand> brandList = await brandRepository.GetAllBrandsAsync();
+
+            GetBrandsResult result = new GetBrandsResult(brandList);
+
+            return result;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Application/Queries/BrandQueries/GetBrandsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Responses.BrandResponses;
+using Catalog.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Queries.BrandQueries
+{
+    public record GetBrandsQuery : IRequest<GetBrandsResult>
+    {
+    }
+}
