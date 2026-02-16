ece29a7936cb82264bfd262f1a15ab6f7562d358
Bushuev Igor <boommotivator@gmail.com>
2026-02-16 19:22:53 +0300

    рефакторинг BrandsController через ApiController

---
+++ src/Services/Catalog/Catalog.API/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private IMediator? mediator;
+
+        protected IMediator Mediator =>
+            mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("служба IMediator недоступна");
+    }
+}
--- src/Services/Catalog/Catalog.API/Controllers/BrandsController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/BrandsController.cs
@@ -6,14 +6,12 @@ using Catalog.Application.Queries.BrandQueries;
 
 namespace Catalog.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BrandsController(IMediator mediator) : ControllerBase
+    public class BrandsController(IMediator mediator) : ApiController
     {
         [HttpGet]
         public async Task<ActionResult<GetBrandsResult>> GetBrand()
         {
-            return await mediator.Send(new GetBrandsQuery());
+            return await Mediator.Send(new GetBrandsQuery());
         }
     }
 }
