8276a25f54e8dca0e542d80874fcfa0c7fc3af84
Bushuev Igor <boommotivator@gmail.com>
2026-02-17 18:49:20 +0300

    CQRS : запрос на получение по айди - CatalogItemByID + создание контроллера

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogItemByIdController.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Application.Queries.CatalogItemQueries;
+using Catalog.Application.Responses.CatalogItemResponses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+
+    public class CatalogItemByIdController(IMediator mediator) : ApiController
+    {
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(GetCatalogItemByIdResult), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<GetCatalogItemByIdResult>> GetCatalogItemById(Guid id)
+        {
+            var result = await Mediator.Send(new GetCatalogItemByIdQuery(id));
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/GetCatalogItemByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Queries.CatalogItemQueries;
+using Catalog.Application.Responses.CatalogItemResponses;
+using Catalog.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.CatalogItemHandlers
+{
+    public class GetCatalogItemByIdQueryHandler(ICatalogItemRepository catalogItemRepository)
+        : IRequestHandler<GetCatalogItemByIdQuery, GetCatalogItemByIdResult>
+    {
+        public async Task<GetCatalogItemByIdResult> Handle(GetCatalogItemByIdQuery query, CancellationToken cancellationToken)
+        {
+            var catalogItem = await catalogItemRepository.GetCatalogItemAsync(query.Id);
+            var result = new GetCatalogItemByIdResult(catalogItem);
+            return result;
+            
+
+            
+        }
+    }
+}
