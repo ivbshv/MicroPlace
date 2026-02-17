99ed908b997fb4c99a0b9c6c9bb32dbd2d0c2d23
Bushuev Igor <boommotivator@gmail.com>
2026-02-17 21:14:21 +0300

    в CreateCatalogItemHandler добавил комментарий для возможной реализации валидации

--- src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/CreateCatalogItemHandler.cs
+++ src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/CreateCatalogItemHandler.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Catalog.Application.Handlers.CatalogItemHandlers
 {
-    internal class CreateCatalogItemHandler(ICatalogItemRepository catalogItemRepository)
+    public class CreateCatalogItemHandler(ICatalogItemRepository catalogItemRepository)
         : IRequestHandler<CreateCatalogItemCommand, CreateCatalogItemResult>
     {
         public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command, CancellationToken cancellationToken)
@@ -24,3 +24,40 @@ namespace Catalog.Application.Handlers.CatalogItemHandlers
         }
     }
 }
+
+
+// public class CreateCatalogItemHandler(
+//     ICatalogItemRepository catalogItemRepository,
+//     IBrandRepository brandRepository,
+//     ICategoryRepository categoryRepository)
+//     : IRequestHandler<CreateCatalogItemCommand, CreateCatalogItemResult>
+// {
+//     public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command, 
+//         CancellationToken cancellationToken)
+//     {
+//         // Проверяем Brand
+//         var brandExists = command.Brand != null
+//             && (await brandRepository.GetAllBrandsAsync())
+//             .Any(b => b.Id == command?.Brand?.Id);
+
+//         if (!brandExists)
+//         {
+//             throw new ArgumentException("Указанный бренд не существует");
+//         }
+
+//         // Проверяем Category
+//         var categoryExists = command.Category != null
+//             && (await categoryRepository.GetAllCategoriesAsync())
+//             .Any(c => c.Id == command.Category.Id);
+
+//         if (!categoryExists)
+//         {
+//             throw new ArgumentException("Указанная категория не существует");
+//         }
+
+//         var catalogItem = command.Adapt<CatalogItem>();
+//         catalogItem.Id = Guid.NewGuid();
+//         await catalogItemRepository.CreateCatalogItemAsync(catalogItem);
+//         return new CreateCatalogItemResult(catalogItem.Id);
+//     }
+// }
