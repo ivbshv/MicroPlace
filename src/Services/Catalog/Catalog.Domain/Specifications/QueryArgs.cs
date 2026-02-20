2b6c09b3766d549a355be630445911bb2b3d0acf
Bushuev Igor <boommotivator@gmail.com>
2026-02-20 19:55:34 +0300

    создал модель QueryArgs с поддержкой фильтрации, поиска и сортировки

---
+++ src/Services/Catalog/Catalog.Domain/Specifications/QueryArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Specifications
+{
+    public record QueryArgs(
+        int PageIndex = 1,
+        int PageSize = 5,
+        Guid? BrandId = null,
+        Guid? CategoryId = null,
+        string? Sort = null,
+        string Search = null
+    )
+    {
+        private const int MaxPageSize = 25;
+
+        public int PageSize { get; init; } = PageSize > MaxPageSize ? MaxPageSize : PageSize;
+    }
+}
