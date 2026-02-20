7df95987006d505a18ed2c2eb0ecefd411bc32a3
Bushuev Igor <boommotivator@gmail.com>
2026-02-20 19:36:29 +0300

    Добавил Swagger аннотации, документацию и UI для v2 API

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogItemControllerV2.cs
@@ -0,0 +1,29 @@
+﻿using Asp.Versioning;
+using Catalog.Application.Queries.CatalogItemQueries;
+using Catalog.Application.Responses.CatalogItemResponses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("2")]
+    [ApiExplorerSettings(GroupName = "v2")]
+    [Route("api/v{version:apiVersion}/CatalogItem")]
+    public class CatalogItemControllerV2(IMediator mediator) : ApiController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(GetCatalogItemsResultV2), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(Tags = new[] { "CatalogItemControllerV2" } )]
+        public async Task<ActionResult<GetCatalogItemsResultV2>> GetCatalogItems(
+            [FromQuery] int pageIndex = 1,
+            [FromQuery] int pageSize = 5
+        )
+        {
+            var query = new GetCatalogItemsQueryV2(pageIndex, pageSize);
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/DependencyInjection.cs
+++ src/Services/Catalog/Catalog.API/DependencyInjection.cs
@@ -36,6 +36,13 @@ public static class DependencyInjection
                 Title = "Catalog API",
                 Version = "v1"
             });
+            config.SwaggerDoc("v2", new OpenApiInfo
+            {
+                Title = "Catalog API",
+                Version = "v2"
+            });
+
+            config.EnableAnnotations();
         });
 
         var licenseKey = configuration.GetSection("Mediatr:LicenseKey").Value;
@@ -54,7 +61,11 @@ public static class DependencyInjection
     {
         app.MapControllers();
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API v1");
+            options.SwaggerEndpoint("/swagger/v2/swagger.json", "Catalog API v2");
+        });
 
         app.MapGet("/", () => "Hello World!");
 
