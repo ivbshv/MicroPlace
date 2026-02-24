0a2a77d7da2046713496f15b1f6625024d754c2b
Bushuev Igor <boommotivator@gmail.com>
2026-02-24 10:07:36 +0300

    Создал проект Common.Kernel, базовые интерфейсы CQRS: команд и запросов в Common.Kernel

---
+++ src/Shared/Common.Kernel/CQRS/Commands/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Common.Kernel.CQRS.Commands
+{
+    public interface ICommandHandler<in  TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    { }
+
+    public interface ICommandHandler<in TCommand>
+        : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit> 
+    { }
+
+    
+}
---
+++ src/Shared/Common.Kernel/CQRS/Queries/IQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Kernel.CQRS.Queries
+{
+    public interface IQueryHandler<in TQuery, TResponse> 
+        : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
