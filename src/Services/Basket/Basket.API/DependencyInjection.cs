c3d9c435cca7ef4665a3ab1b2e96930af5e1b234
Bushuev Igor <boommotivator@gmail.com>
2026-02-24 10:44:30 +0300

    Добавил файлы DI и GlobalUsings, зарегистрировал зависимости для API слоя и настроил конвейер обработки запросов.

---
+++ src/Services/Basket/Basket.API/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApiServices(
+            this IServiceCollection services,
+            IConfiguration configuration 
+        )
+        {
+            return services;
+        }
+
+        public static WebApplication UseApiServices(
+            this WebApplication app
+        )
+        {
+            return app;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,11 @@
+using Basket.API;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApiServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseApiServices();
 
 app.Run();
