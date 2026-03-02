419ca496ab62ede30a3bf489f2cfb18f267937cb
Bushuev Igor <boommotivator@gmail.com>
2026-03-02 18:39:00 +0300

    добавил доменную модель Promo для промоакций

---
+++ src/Services/Promotion/Promotion.Grpc/Domain/Promo.cs
@@ -0,0 +1,10 @@
+﻿namespace Promotion.Grpc.Domain
+{
+    public class Promo
+    {
+        public Guid Id { get; set; }
+        public string? CatalogItemId { get; set; }
+        public string? Title { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+    }
+}
