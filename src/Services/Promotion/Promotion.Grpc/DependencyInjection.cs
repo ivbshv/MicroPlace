956fb81e0fd78f2ddea1e228d29d8646436ca44a
Bushuev Igor <boommotivator@gmail.com>
2026-03-02 18:33:34 +0300

    Настройка MySQL базы данных с Dapper ORM

--- src/Services/Promotion/Promotion.Grpc/DependencyInjection.cs
+++ src/Services/Promotion/Promotion.Grpc/DependencyInjection.cs
@@ -7,8 +7,23 @@
             IConfiguration configuration
         )
         {
+            var mySqlConnection = configuration.GetConnectionString("MySqlConnection");
+
+            services.AddScoped<IDbConnection>(_ =>
+                new MySqlConnection(mySqlConnection));
+
             services.AddGrpc();
             services.AddGrpcReflection();
+
+            var assembly = typeof(Program).Assembly;
+            var licenseKey = configuration.GetSection("Mediatr:LicenseKey").Value;
+
+            services.AddMediatR(config =>
+            {
+                config.LicenseKey = licenseKey;
+                config.RegisterServicesFromAssemblies(assembly);
+                
+            });
 
             return services;
         }
--- src/Services/Promotion/Promotion.Grpc/GlobalUsings.cs
+++ src/Services/Promotion/Promotion.Grpc/GlobalUsings.cs
@@ -2,4 +2,9 @@
 
 global using Promotion.Grpc.Services;
 global using Promotion.Grpc;
+global using Common.Kernel.Behaviors;
+global using MySqlConnector;
+global using System.Data;
+
+
 
